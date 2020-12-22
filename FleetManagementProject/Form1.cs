7bbcd6809ab3c15338021208a1db2b158b07f5c7
DTeeam <teamdetecnik@gmail.com>
2020-12-22 14:46:32 +0100

    Project started

--- FleetManagementProject/Form1.cs
+++ FleetManagementProject/Form1.cs
@@ -16,5 +16,10 @@ namespace FleetManagementProject
         {
             InitializeComponent();
         }
+
+        private void buttonToAdd_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Veichle successfully added.");
+        }
     }
 }
